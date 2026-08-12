c7246145a60846f3c06fc61daa54adfd46c6e75e
elcucuy040918-maker <elcucuy040918@gmail.com>
2026-08-13 05:13:47 +0900

    Add files via upload

--- functions/api/upload.js
+++ functions/api/upload.js
@@ -7,7 +7,6 @@ export async function onRequestPost(context) {
     httpMetadata: { contentType: file.type }
   });
 
-  // Step 2에서 설정한 R2 퍼블릭 URL로 변경하세요.
-  const imageUrl = `https://pub-8896443c88344341914c8996ecc812f9.r2.dev/{fileName}`; 
+  const imageUrl = `https://pub-8896443c88344341914c8996ecc812f9.r2.dev/${fileName}`;
   return Response.json({ url: imageUrl });
 }
